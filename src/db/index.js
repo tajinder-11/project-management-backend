934bb3943c2f81a61168201836716728e4122ede
tajinder-11 <dheertajinder@gmail.com>
2026-07-08 22:44:46 +0530

    chore: changed log message

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected", process.env.MONGO_URI);
+    console.log("MongoDB connected");
   } catch (error) {
     console.log("MongoDB connection error", error);
     process.exit(1);
