b2eb34bf06c9dde62def5b695cabd869056e5b3a
tajinder-11 <dheertajinder@gmail.com>
2026-07-18 00:06:24 +0530

    chore: added verify jwt middleware and created logout api

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -124,4 +124,26 @@ const login = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, login };
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: "",
+      },
+    },
+    {
+      new: true,
+    },
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"));
+});
+
+export { registerUser, login, logoutUser };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "Express";
-import { registerUser } from "../controllers/auth.controllers.js";
+import { logoutUser, registerUser } from "../controllers/auth.controllers.js";
 import { login } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userLoginValidator,
   userRegisterValidator,
 } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/logout").post(verifyJWT, logoutUser);
 export default router;
